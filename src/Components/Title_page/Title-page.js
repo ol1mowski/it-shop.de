f6d1cc3197c3567fbdfe1316ad31146bcc7233f4
Ol1mowski <olimarki123@wp.pl>
2023-08-21 17:26:26 +0200

    add some stuff to main site

--- src/Components/Title_page/Title-page.js
+++ src/Components/Title_page/Title-page.js
@@ -1,7 +1,33 @@
 import style from './Title-page.module.scss';
 import logo from '../../assets/icons/company_logo.jpg';
+import hamburger from '../../assets/icons/hamburger_menu.svg';
+import { useEffect, useRef, useState } from 'react';
 
 const TitlePage = () => {
+
+    const hamburgerBtn = useRef(null);
+    const menu = useRef(null);
+
+    const [click, setClick] = useState(false);
+
+    useEffect(() => {
+
+        const clickHamburgerHandler = () => {
+            click ? setClick(false) : setClick(true);
+            click ? menu.current.style.display = 'block' : menu.current.style.display = 'none';
+            menu.current.addEventListener('animationend', () => {
+                click ? menu.current.style.transform = 'translateY(0)' : menu.current.style.transform = 'translateY(-100vh)';          
+            });
+        }
+
+        hamburgerBtn.current.addEventListener('click', clickHamburgerHandler);
+
+        return (() => {
+            hamburgerBtn.current.removeEventListener('click', clickHamburgerHandler);
+        });
+
+    });
+
     return (
         <>
             <header className={style.titlePage__header}>
@@ -9,18 +35,23 @@ const TitlePage = () => {
                     <img className={style.titlePage__header__iconLogo__img} src={logo} alt='Companies logo' />
                 </div>
                 <section className={style.titlePage__header__menu}>
-                    <div className={style.titlePage__header__menu__wrapper}>
-                        <div className={style.titlePage__header__menu__items}>
-                            About
-                        </div>
-                        <div className={style.titlePage__header__menu__items}>
-                            What you gain
-                        </div>
-                        <div className={style.titlePage__header__menu__items}>
-                            Offerts
-                        </div>
-                        <div className={style.titlePage__header__menu__items}>
-                            Contact
+                    <div className={style.titlePage__header__menu__hamburger}>
+                        <img ref={hamburgerBtn} src={hamburger} alt='hamburger-menu' className={style.titlePage__header__menu__hamburger__img} />
+                    </div>
+                    <div ref={menu} className={style.titlePage__header__menu__wrapper}>
+                        <div className={style.titlePage__header__menu__container}>
+                            <div className={style.titlePage__header__menu__items}>
+                                About
+                            </div>
+                            <div className={style.titlePage__header__menu__items}>
+                                What you gain
+                            </div>
+                            <div className={style.titlePage__header__menu__items}>
+                                Offerts
+                            </div>
+                            <div className={style.titlePage__header__menu__items}>
+                                Contact
+                            </div>
                         </div>
                     </div>
                 </section>
