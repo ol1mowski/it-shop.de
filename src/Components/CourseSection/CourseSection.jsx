6230ad9db09eb74c5648fabced26a01a9285d8c5
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2023-10-27 10:06:02 +0200

    add skliders button

--- src/Components/CourseSection/CourseSection.jsx
+++ src/Components/CourseSection/CourseSection.jsx
@@ -1,11 +1,15 @@
 import CoursesNavigation from './CoursesNavigation/CoursesNavigation';
 import CoursesSlider from './CoursesSlider/CoursesSlider';
+import style from './courseSection.module.scss';
 
 const CourseSection = () => {
     return (
         <>
+        <section className={style.main}>
+
            <CoursesNavigation />
            <CoursesSlider />
+        </section>
         </>
     );
 };
--- src/Components/CourseSection/CoursesSlider/CoursesSlider.jsx
+++ src/Components/CourseSection/CoursesSlider/CoursesSlider.jsx
@@ -1,36 +1,36 @@
 import style from "./coursesSlider.module.scss";
+import arrow from '../../../assets/icons/arrow.svg';
 
 const CoursesSlider = () => {
   return (
     <>
       <div class="d-flex align-items-center justify-content-between pb-4 mb-3">
-        <h2 id={style.h1} class="h1 mb-0 me-3">Popular Courses</h2>
+        <h2 id={style.h1} class="h1 mb-0 me-3">
+          Popular Courses
+        </h2>
         <div class="d-flex">
           <button
             type="button"
-            id="popular-prev"
-            class="btn btn-prev btn-icon btn-sm me-2"
             aria-label="Previous slide"
             tabindex="0"
+            className={style.button}
             aria-controls="swiper-wrapper-e7eb2da919b6c945"
             aria-disabled="false"
           >
-            <i class="bx bx-chevron-left"></i>
+            <img src={arrow} alt="Arrow icon"  id={style.left}className={style.arrow} />
           </button>
           <button
             type="button"
-            id="popular-next"
-            class="btn btn-next btn-icon btn-sm ms-2"
+            className={style.button}
             aria-label="Next slide"
             tabindex="0"
             aria-controls="swiper-wrapper-e7eb2da919b6c945"
             aria-disabled="false"
           >
-            <i class="bx bx-chevron-right"></i>
+            <img src={arrow} alt="Arrow icon" id={style.right} className={style.arrow} />
           </button>
         </div>
       </div>
-      
     </>
   );
 };
