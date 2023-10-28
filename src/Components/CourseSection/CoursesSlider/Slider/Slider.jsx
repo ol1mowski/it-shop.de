cd62fef2d6d72e12c5865b04116b374999c10988
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2023-10-28 19:52:36 +0200

    add finish sketch to course card

--- src/Components/CourseSection/CoursesSlider/Slider/Slider.jsx
+++ src/Components/CourseSection/CoursesSlider/Slider/Slider.jsx
@@ -1,22 +1,30 @@
-import style from './slider.module.scss';
-import img from '../../../../assets/img/slider_image/01.jpg';
-import icon from '../../../../assets/icons/save.svg';
-import clock from '../../../../assets/icons/clock.svg';
-import like from '../../../../assets/icons/like.png';
+import style from "./slider.module.scss";
+import img from "../../../../assets/img/slider_image/01.jpg";
+import icon from "../../../../assets/icons/save.svg";
+import clock from "../../../../assets/icons/clock.svg";
+import like from "../../../../assets/icons/like.png";
 
 const Slider = () => {
   return (
     <>
       <div className={style.slider}>
         <div className={style.slider__imgSection}>
-          <img src={img} alt='courses ' className={style.slider__imgSection__img} />
+          <img
+            src={img}
+            alt="courses "
+            className={style.slider__imgSection__img}
+          />
           <div className={style.slider__imgSection__label}>
             <span className={style.slider__imgSection__label__span}>
               Best Seller
             </span>
           </div>
           <div className={style.slider__imgSection__save}>
-            <img src={icon} className={style.slider__imgSection__save__icon} alt='save icon' />
+            <img
+              src={icon}
+              className={style.slider__imgSection__save__icon}
+              alt="save icon"
+            />
           </div>
         </div>
 
@@ -28,29 +36,23 @@ const Slider = () => {
             <p className={style.slider__contentSection__pAutor}>
               By Albert Flores
             </p>
-            <p className={style.slider__contentSection__pPrice}>
-              $12.50
-            </p>
+            <p className={style.slider__contentSection__pPrice}>$12.50</p>
           </div>
         </div>
-        
-        <div className={style.slider__iconsSection}>
-          {/* <div className={style.slider__iconsSection__clock}>
-            <img src={clock} className={style.slider__iconsSection__clock__img} alt='clock icon' />
-            <p className={style.slider__iconsSection__clock__string}>
-              220 hours
-            </p>
+
+        <div class="card-footer d-flex align-items-center p-1 fs-sm text-muted py-4">
+          <div class="d-flex align-items-center me-4">
+            <img src={clock} alt="clock icon" className={style.clock} />
+            220 hours
           </div>
-          <div className={style.slider__iconsSection__clock}>
-            <img src={like} className={style.slider__iconsSection__clock__img} alt='clock icon' />
-            <p className={style.slider__iconsSection__clock__string}>
-              220 hours
-            </p>
-          </div> */}
+          <div class="d-flex align-items-center">
+            <img src={like} alt="clock icon" className={style.like} />
+            94% (4.2K)
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Slider;
